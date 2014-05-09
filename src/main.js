66b2e74648a6fc333f8b12673b388aa2ba978e22
franleplant <franleplant@gmail.com>
2014-05-08 22:05:54 -0300

    add file navigator

--- src/main.js
+++ src/main.js
@@ -31,7 +31,11 @@ app.controller('HomeCtrl', function () {
 });
 
 
-app.controller('AppCtrl', function ($scope, $routeParams, $http, $sce, $interval) {
+app.controller('AppCtrl', function ($scope, $routeParams, $http, $sce, $interval, Gist) {
+
+
+
+
 
 
 	function markdown_to_noter() {
@@ -70,32 +74,86 @@ app.controller('AppCtrl', function ($scope, $routeParams, $http, $sce, $interval
 
 
 
-app.controller('NavigatorCtrl', function ($scope, $location) {
-
-	$scope.url = $scope.url || "https://gist.github.com/franleplant/386ddfa1b663cd2f9f19#file-notertest-a-md";
-
-	this.parse_and_redirect = function () {
-		var url = $scope.url;
-		var r = /(?:http(?:s{0,1}))(?:\:\/\/)(?:gist\.github\.com\/)([^\/]*)(?:\/)([^#]*)(?:#)(.*)$/;
-		var parsed = url.match(r);
+app.controller('NavigatorCtrl', function ($scope, $location, Gist) {
 
 
-		var user = parsed[1];
+	$scope.url = $scope.url || "https://gist.github.com/franleplant/386ddfa1b663cd2f9f19";
+
+	function id_from_url (url) {
+
+		var r = /(?:http(?:s{0,1}))(?:\:\/\/)(?:gist\.github\.com\/)([^\/]*)(?:\/)(.*)$/;
+		var parsed = url.match(r);	
+
 		var gist_id = parsed[2];
-		var raw_file_name = parsed[3];
-		var file_name = raw_file_name.match(/(?:file-)([^md]*)(?:-)(.{1,3})$/)[1] + '.md';
 
-		console.log(user, gist_id, file_name);
-
-
-		var l = '/gist/' + gist_id + '/' + file_name;
-		console.log(l);
-		$location.path(l);
-
+		return gist_id;
 	}
+
+	var gist_id;
+
+	this.go = function () {
+		gist_id = id_from_url($scope.url);
+		Gist(gist_id).then(function (gist) {
+			$scope.files = gist.files();
+		});
+	}
+
+	this.go_to_file = function(file) {
+		$location.path('/gist/' + gist_id + '/' + file.filename);
+	}
+	// this.parse_and_redirect = function () {
+	// // 	var url = $scope.url;
+	// 	var r = /(?:http(?:s{0,1}))(?:\:\/\/)(?:gist\.github\.com\/)([^\/]*)(?:\/)([^#]*)(?:#)(.*)$/;
+	// 	var parsed = url.match(r);
+
+
+	// 	var user = parsed[1];
+	// 	var gist_id = parsed[2];
+	// 	var raw_file_name = parsed[3];
+	// 	var file_name = raw_file_name.match(/(?:file-)([^md]*)(?:-)(.{1,3})$/)[1] + '.md';
+
+	// 	console.log(user, gist_id, file_name);
+
+
+	// 	var l = '/gist/' + gist_id + '/' + file_name;
+	// 	console.log(l);
+	// 	$location.path(l);
+
+	// }
 
 });
 
+
+app.service('Gist', function ($http) {
+
+	var Gist = function (data) {
+		var that = {};
+
+
+		that.raw = function () {
+			return data;
+		};
+
+		that.files = function () {
+			return data.files;
+		}
+
+		that.file = function (name) {
+			return data.files[name];
+		}
+
+		return that;
+	};
+
+
+	return function get_gist(id) {
+		return $http.get('https://api.github.com/gists/' + id)
+			.then(function (response) {
+				return Gist(response.data);
+			});
+	}
+
+});
 
 //dynamic rendering math http://docs.mathjax.org/en/latest/typeset.html
 
